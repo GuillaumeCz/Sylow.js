ff6a1c3056058d4df391e3432e718f7e8f8b6e81
GuillaumeCz <guillaume.couzy@gmail.com>
2018-09-15 15:48:30 +0200

    Add tests

---
+++ test/sylow.test.js
@@ -0,0 +1,19 @@
+/* eslint-env node, mocha */
+
+import assert from 'assert';
+
+// import Sylow from './lib/sylow';
+
+describe('Sylow', () => {
+  describe('...Connection', () => {
+    it('...should return a instance of class Sylow', () => {
+      assert.equal('Hello', 'Hello');
+    });
+  });
+
+  describe('TestMe', () => {
+    it('...should go ok', () => {
+      assert.equal(1, 2 - 1);
+    });
+  });
+});
